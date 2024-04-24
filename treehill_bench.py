7f23f7198c2185d0e24e6388d0b0f32ee8d21cd8
Heng <31088159+shiukaheng@users.noreply.github.com>
2024-04-24 16:50:40 +0100

    Create treehill_bench.py

---
+++ treehill_bench.py
@@ -0,0 +1,32 @@
+import torch
+from gs.core.GaussianModel import GaussianModel
+from gs.geometry.grid import Grid
+from gs.io.ply import read_ply
+from gs.io.colmap import load
+from gs.trainers.grid.train import train
+from gs.trainers.grid.config import GridTrainConfig
+
+if __name__ == "__main__":
+    cameras, sparse = load("./datasets/mip_nerf_360/treehill/")
+    
+    input_model = GaussianModel.from_point_cloud(sparse).cuda()
+
+    output_model = train(
+    input_model, 
+    cameras, 
+    GridTrainConfig(
+        iterations=10000,
+        densify_until_iter=8000,
+        grid=Grid(40, 
+        grid_origin=torch.Tensor([0,0,0])), 
+        sync_interval=100,
+        densify_interval=100,
+        # scales_lr=0.02,
+        # rotations_lr=0.02,
+        extra_cell_compensation="last",
+        # preview_camera=cameras[266],
+        min_gaussians=500,
+    ))
+
+    # Save model
+    output_model.save_ply("./samples/treehill_bench.ply")
