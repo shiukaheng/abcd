24b984243dfaca48d499955cd8c45f0eddc1e1d1
Heng <31088159+shiukaheng@users.noreply.github.com>
2026-08-27 02:59:17 +0100

    harden dataset download script

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,4 +1,7 @@
-mkdir datasets
-wget -O ./datasets/mip_nerf_360.zip https://skhpersonal.s3.amazonaws.com/mip_nerf_360.zip
-unzip ./datasets/mip_nerf_360.zip -d ./datasets
-rm ./datasets/mip_nerf_360.zip
+#!/usr/bin/env bash
+set -euo pipefail
+
+mkdir -p datasets
+wget -O datasets/mip_nerf_360.zip https://skhpersonal.s3.amazonaws.com/mip_nerf_360.zip
+unzip datasets/mip_nerf_360.zip -d datasets
+rm datasets/mip_nerf_360.zip
